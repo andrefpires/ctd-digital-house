37dadd153618454ee14e80a863309cf89bf0a667
andrefpires <andrefpires1@gmail.com>
2022-05-25 16:17:39 -0300

    Módulo 01 - Introdução à POO | Aula 01 - Java

---
+++ bimestre-03/programacao-orientada-objetos/modulo01-introducao/aula01-java/Main.java
@@ -0,0 +1,16 @@
+// package com.company;
+
+public class Main {
+  public static void main(String[] args) {
+  /*
+  O método "main" é uma função que indica o início do programa, tudo que  escrevemos dentro desta função será
+  executado, pela Java Virtual Machine, quando o programa for chamado. Por convenção o método precisa ser público, estático,
+  não pode retornar nada (void), precisa receber um array do tipo string por argumento e possuir o nome main.
+  */
+    int num = 25;
+
+    System.out.printIn("Hoje é dia" + num); // Imprimi no console
+  }
+}
+
+// O nome do arquivo e da classe devem ser os mesmo. É uma boa prática que seu nome comece com letra maiúscula
